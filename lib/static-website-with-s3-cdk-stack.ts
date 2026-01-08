608fd7452c720ad95dfeabdb1a0a48783bc6a793
Marcio Cruz de Almeida <marciocadev@gmail.com>
2026-01-08 17:27:42 -0300

    adding licence file

--- lib/static-website-with-s3-cdk-stack.ts
+++ lib/static-website-with-s3-cdk-stack.ts
@@ -1,16 +1,30 @@
-import * as cdk from 'aws-cdk-lib/core';
+import { BlockPublicAccess, Bucket } from 'aws-cdk-lib/aws-s3';
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
+import { CfnOutput, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { join } from 'path';
 
-export class StaticWebsiteWithS3CdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class StaticWebsiteWithS3CdkStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucket = new Bucket(this, "static-website-with-s3", {
+      bucketName: "static-website-with-cdk",
+      websiteIndexDocument: "index.html",
+      publicReadAccess: true,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ACLS_ONLY,
+      autoDeleteObjects: true,
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'StaticWebsiteWithS3CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new BucketDeployment(this, "deploy-website", {
+      sources: [Source.asset(join(__dirname, "./website"))],
+      destinationBucket: bucket
+    });
+
+    new CfnOutput(this, "website-url", {
+      value: bucket.bucketWebsiteUrl,
+      exportName: "static-website-with-s3-url"
+    })
   }
 }
